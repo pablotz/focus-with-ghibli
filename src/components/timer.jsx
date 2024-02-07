2231d6d946825bba5bb13d92bd33ef57f666f2f1
Pablo Hernández <pablotroll789@hotmail.com>
2024-02-07 01:39:10 -0600

    Cleaning Timer logic from component

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,46 +1,20 @@
 import '../assets/styles/timer.css'
-import { useState, useEffect } from 'react'
-import calculateTimer from '../utils/calculateTimer';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleActive, setMinutes, setSeconds } from '../redux/slices/timerSlice';
+import { timerControl, timerWork } from '../utils/calculateTimer';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 const Timer = () => {
 
     const { isActive, minutes, seconds, selectedMinutes } = useSelector(state => state.timer);
-    const dispatch = useDispatch();
-  
-    const getTime = (deadline) => {
-        const time = Date.parse(deadline) - Date.now();
-        setMinutes();
-        dispatch(setMinutes(Math.floor((time / 1000 / 60) % 60)))
-        dispatch(setSeconds(Math.floor((time / 1000) % 60)));
-    };
-  
-    
-    const timerControl = () => {
-        if(selectedMinutes < 10) return;
-        if(isActive){
-            dispatch(toggleActive())
-        } else {
-            dispatch(toggleActive())
-            setMinutes(0)
-            setSeconds(0)
-        }
-        
-    }
 
     useEffect(() => {
-        
         if(isActive) {
-            let deadline = calculateTimer(selectedMinutes)
-            getTime(deadline);
-            const interval = setInterval(() => getTime(deadline), 1000);
-            return () => clearInterval(interval);
+            timerWork()
         }
     }, [isActive, selectedMinutes]);
 
   return (
-    <div id={isActive ? "start-timer" : "idle-timer"}   className='timer-container pt-80 flex items-center justify-center'>
+    <div className={`${isActive ? "start-timer pt-80" : "idle-timer pt-96"} timer-container  flex items-center   justify-center`}>
         <div>
             <div>
                 <h1
--- src/utils/calculateTimer.js
+++ src/utils/calculateTimer.js
@@ -1,11 +1,58 @@
-export default (minutes) => {
+import { setMinutes, setSeconds, toggleActive } from "../redux/slices/timerSlice";
+import { store } from "../redux/store";
+
+let { dispatch, getState } = store;
+const { isActive } = getState();
+
+export const getDeadline = (minutes) => {
     // The min time will be 10 minutes
     // 10 - 10 minutes
     // 11 - 11 minutes
     // 12 - ...
     let date = new Date();
-    date.setMinutes(date.getMinutes() + minutes);
+    // date.setMinutes(date.getMinutes() + minutes);
+    date.setSeconds(date.getSeconds() + 10);
     console.log(date)
     return date;
+}
+
+export const getTime = (deadline) => {
+    const time = Date.parse(deadline) - Date.now();
+    let minutes = Math.floor((time / 1000 / 60) % 60);
+    let seconds = Math.floor((time / 1000) % 60);
+    if(minutes >= 0 || seconds >= 0) {
+        dispatch(setMinutes(minutes))
+        dispatch(setSeconds(seconds));
+    } else {
+        return 'finish';
+    }
+};
+
+export const timerWork = () => {
+     // let deadline = getDeadline(selectedMinutes)
+    let deadline = getDeadline()
+    getTime(deadline);
+
+    const interval = setInterval(() => {
+        let time = getTime(deadline)
+        console.log(time)
+        if(time === 'finish') {
+            clearInterval(interval);
+        }
+    }, 1000
+    );
+    return () => clearInterval(interval);
+}
+
+export const timerControl = () => {
+    // if(selectedMinutes < 10) return;
+    if(isActive){
+        dispatch(toggleActive())
+    } else {
+        dispatch(toggleActive())
+        setMinutes(0)
+        setSeconds(0)
+    }
+    
 }
 
