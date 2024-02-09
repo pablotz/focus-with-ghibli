671635720ba6b2f67de88e77be082fb253e62b08
Pablo Hernandez <pablotroll789@hotmail.com>
2024-02-08 20:36:10 -0600

    Button Style

--- src/components/controlTimer.jsx
+++ src/components/controlTimer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import '../assets/styles/button.css'
 import { timerControl } from '../utils/calculateTimer'
+import { useSelector } from 'react-redux';
 
 const ControlTimer = () => {
-  return (
-        <button onClick={() => timerControl()}>Start</button>
-  )
+    const { isActive } = useSelector(state => state.timer);
+    return (
+            <button className={`button-30 ${isActive ? 'stop' : 'start' }`} onClick={() => timerControl()}>
+                {
+                    isActive ? 
+                    'Stop' : 'Start'
+                }
+            </button>
+    )
 }
 
 export default ControlTimer
--- src/focus.jsx
+++ src/focus.jsx
@@ -6,7 +6,7 @@ import ControlTimer from './components/controlTimer.jsx';
 const Focus = () => {
   const { isActive } = useSelector(state => state.timer);
   return (
-    <div className={`${isActive ? "start-timer pt-80" : "idle-timer pt-96"} timer-container  flex justify-center `}>
+    <div className={`${isActive ? "start-timer pt-80" : "idle-timer pt-96"} timer-container w-full flex flex-col place-content-center place-items-center  `}>
         <div>
           <Timer />
         </div>
