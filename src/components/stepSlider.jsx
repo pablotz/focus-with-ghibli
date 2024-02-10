0d4357010444a84683a81c2ce479b79e350d9edc
pablotz <pablotroll789@hotmail.com>
2024-02-09 23:00:01 -0600

    Slider Added

---
+++ src/components/stepSlider.jsx
@@ -0,0 +1,52 @@
+import { styled } from '@mui/material';
+import Slider from '@mui/material/Slider';
+import { useDispatch, useSelector } from 'react-redux';
+import { setSelectedMinutes } from '../redux/slices/timerSlice';
+import { useEffect, useState } from 'react';
+
+
+const PrettoSlider = styled(Slider)({
+    color: '#F5F5DC',
+    height: 10,
+    '& .MuiSlider-track': {
+      border: 'none',
+      backgroundColor: '#ffffd1'
+    },
+    '& .MuiSlider-thumb': {
+      height: 24,
+      width: 24,
+      backgroundColor: '#F5F5DC',
+      border: '2px solid currentColor',
+      '&:focus, &:hover, &.Mui-active, &.Mui-focusVisible': {
+        boxShadow: 'inherit',
+      },
+      '&::before': {
+        display: 'none',
+      },
+    },
+});
+
+const StepSlider = () => {
+    const { selectedMinutes } = useSelector(state => state.timer);
+    const dispatch = useDispatch();
+    
+
+    const handleTimerChange = (value) => {
+        dispatch(setSelectedMinutes(value))
+    }
+    
+    return (
+        <PrettoSlider
+            value={selectedMinutes}
+            min={10}
+            max={60}
+            step={5}
+            valueLabelDisplay="off"
+            onChange={(e) => {
+                handleTimerChange(e.target.value)
+            }}
+        />
+    )
+}
+
+export default StepSlider
--- src/focus.jsx
+++ src/focus.jsx
@@ -1,16 +1,22 @@
 import { useSelector } from 'react-redux';
 import Timer from './components/timer'
 import ControlTimer from './components/controlTimer.jsx';
-
+import StepSlider from './components/stepSlider.jsx';
+import './assets/styles/timer.css'
 
 const Focus = () => {
   const { isActive } = useSelector(state => state.timer);
   return (
-    <div className={`${isActive ? "start-timer pt-80" : "idle-timer pt-96"} timer-container w-full flex flex-col place-content-center place-items-center  `}>
-        <div>
+    <div className={`${isActive ? "start-timer pt-80" : "idle-timer pt-72"} timer-container`}>
+        <div className='w-full flex flex-col place-content-center place-items-center'>
           <Timer />
         </div>
-        <div>
+        <div className='slider-container' style={{display: isActive ? 'none' : 'block'}}>
+          <StepSlider />
+        </div>
+        <div className='control-container w-full flex flex-col place-content-center place-items-center'
+              style={{marginTop: isActive ? '0em' : '2em'}}
+        >
           <ControlTimer />
         </div>
     </div>
